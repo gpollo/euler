bc435efe8106ad08aebc22e7f610b3331fa8d5a5
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2016-01-24 15:20:34 -0500

    Clean up

--- problems/problem_0001/src/main.c
+++ problems/problem_0001/src/main.c
@@ -8,15 +8,15 @@
 #define MULT_2 5
 
 int main() {
-    int sum, i;
+    int sum = 0;
+    int i;
 
-    sum = 0;
     for(i = 0; i < 1000; i++)
         if(i % MULT_1 == 0 || i % MULT_2 == 0)
             sum += i;
 
     printf("The sum of all the multiples of " STR(MULT_1) " or " STR(MULT_2) " below " STR(MAX)
-           " is %d\n", sum);
+           " is %d.\n", sum);
 
     return 0;
 }
