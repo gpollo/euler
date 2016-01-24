098465403ebc2b78188e3e148249d8f7ad0ee1c8
Gabriel-Andrew Pollo Guilbert <gabriel14_wii@hotmail.com>
2016-01-24 15:42:34 -0500

    Solve problem 2

---
+++ problems/problem_0002/src/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define STR_HELPER(x) #x
+#define STR(x) STR_HELPER(x)
+
+#define MAX 4000000
+
+int main() {
+    int sum = 0;
+    int n[] = {0, 1, 1};
+    int i = 0;
+
+    while(1) {
+        if(n[i] % 2 == 0)
+            sum += n[i];
+
+        if(n[0] > MAX && n[1] > MAX && n[2] > MAX)
+            break;
+
+        switch(i) {
+            case 0:
+                n[0] = n[1]+n[2]; i++;
+                break;
+            case 1:
+                n[1] = n[2]+n[0]; i++;
+                break;
+            case 2:
+                n[2] = n[0]+n[1]; i=0;
+                break;
+        }
+    };
+
+    printf("The sum of the even-valued terms in the fibonacci sequence up to " STR(MAX)
+           " is %d.\n", sum);
+
+    return 0;
+}
